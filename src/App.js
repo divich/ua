d80ac20dadd8e287596aa02c659f6515472fb679
Yuv sinha <yuvsinha01@gmail.com>
2019-06-22 14:15:53 +0530

    second changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from "react";
 import TextField from "material-ui/TextField";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-
+import RaisedButton from "material-ui/RaisedButton";
+import AppBar from "material-ui/AppBar";
+import IconButton from "material-ui/IconButton";
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <h2>Registration Form</h2>
-        <h3 style={{ color: "green" }}>Signup</h3>
+        <AppBar
+          title="Signup Form"
+          iconElementLeft={
+            <IconButton>
+              {this.props.showBackNavigation ? (
+               
+              ) : null}
+            </IconButton>
+          }
+        />
+        ) : null}/>
+        <h2 style={{ display: "flex", justifyContent: "center" }}>
+          {" "}
+          Registration Form
+        </h2>
+        <h3 style={{ color: "green", marginLeft: 20 }}>Signup</h3>
         <div
           style={{
             display: "flex",
             flexDirection: "column",
-            color: "MEdiumSeaGreen"
+            color: "MEdiumSeaGreen",
+            margin: 20
           }}
         >
           <span>NAME: </span> <TextField hintText="Hint Text" />
@@ -21,6 +38,11 @@ class App extends Component {
           <span>Contact: </span> <TextField hintText="Hint Text" /> <br />
           <span>Address: </span> <TextField hintText="Hint Text" /> <br />
         </div>
+        <RaisedButton
+          label="Submit"
+          primary={true}
+          style={{ marginLeft: 50 }}
+        />
       </MuiThemeProvider>
     );
   }
